26b5b3fbfcfa01029204cc034ba9c38b47c799c7
arascch <arash.ask.94@gmail.com>
2018-11-30 05:05:23 -0800

    commit

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from . import models 
 
-# Create your views here.
+
+def index(request) : 
+
+    num_book = models.Book.objects.all().count()
+    num_instance = models.BookInstance.objects.all().count()
+    num_instances_available =models.BookInstance.objects.filter(status_exact = 'a') . count()
+    num_authors = models.Author.objects.count()
+
+    return render(
+        request , 
+        'index.html',
+        context = {'num_books' : num_book , 'num_instances' : num_instance , 'num_instaces_available' : num_instances_available , 'num_authors' : num_authors} , 
+        )
+
+        
+
